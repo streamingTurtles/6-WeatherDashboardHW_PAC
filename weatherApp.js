e79a2e5c0828f0ec9c285f027e561a192d91dbe5
streamingturtles.com <pcardillo@streamingturtles.com>
2020-12-20 04:44:38 -0500

    added new ajax query to get the date for each of the 5 day forcast to populate in its appropriate bootstrap card. Now working on appending the icon, Temp and Humidity into the card

--- weatherApp.js
+++ weatherApp.js
@@ -18,16 +18,16 @@ function saveCitiesSearched(citiesKeyValObjList) {
       cityListEntry.text(cityNamey);  
       $("#citiesInlocalStorage").append(cityListEntry);
     }
-  }
+}
   
   
 
 
 
 
-//   function to get data using API calls - to build out:
-  function getWeatherData(city, citiesKeyValObjList) {
-      
+// function to get data using API calls - to build out:
+function getWeatherData(city, citiesKeyValObjList) {
+    
     console.log("Here is a Cookie: streaming turtles are cool! ");
     // console.log("My API Key from OpenWeatherMap is: e012fbc5ba9141b98a8748d19623cffb ")
     console.log("City Search Query Sring is: https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=e012fbc5ba9141b98a8748d19623cffb&q=city ")
@@ -37,7 +37,7 @@ function saveCitiesSearched(citiesKeyValObjList) {
     // https://openweathermap.org/api/uvi  - documentation
     var longitude; // variable needed for the extra data needed for UV query string
     var latitude ; // variable needed for the extra data needed for UV query string
- 
+
 
     saveCitiesSearched(citiesKeyValObjList);
     // API query string to get the weather object
@@ -45,88 +45,106 @@ function saveCitiesSearched(citiesKeyValObjList) {
     var myQueryAPI = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=e012fbc5ba9141b98a8748d19623cffb&q=" + city;
     console.log("myQueryAPI is: ", myQueryAPI);
 
-    // 1. Top Line Weather
-    // weather data query
-    $.ajax({
-        url: myQueryAPI,
-        method: "GET"
-    }).then(function(weather){  // pass in weather object from return of the myQueryAPI call
-        // using dayJS - show current time dayjs().format() 
-        // console.log("Today is: ", dayjs().format('dddd, MMMM D YYYY' +  ' h mm A'));
-        console.log("Today is: ", dayjs().format('dddd, MMMM D YYYY' +  ' h:mm A'));
-        var currentDateTime = dayjs().format('ddd, M/D/YYYY');
-        console.log(currentDateTime);
-        console.log("current query url is: ", myQueryAPI); // URL query request for weather data
-        console.log("weather data received from request url : ", weather); // object data received from request
-        // adding the City name to render in the html from the weather object - placement in id=#cityName
-        var showDate = $("<h4>"); // jQuery, go make me a h4 tag, & create variable to store element object to render in html markup
-        $("#cityName").empty(); // clear any preExisting data
-        $("#cityName").append(showDate.text(currentDateTime)); // jQuery, go find id and add date into its html tag the id lives
-        var showCity = $("<h4>").text(weather.name); // create variable to store object property weather.name to render in html markup
-        $("#cityName").prepend(showCity)// jQuery, go find id in html and now render in html markup in the beginning of selected tag
-        // adding the City weather icon to render in the html from the weather object - placement in id=#cityWeatherIcon
-        // https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon - FYI: how to overview to implement rendering the icon
-        var showIcon = $("<img>").attr("src","https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png");
-        $("#cityWeatherIcon").empty(); // clear any preExisting data
-        $("#cityWeatherIcon").append(showIcon); // render to html at id = #cityWeatherIcon
-        // adding in the temperature, humidity, windSpeed & UV index
-        $("#cityTemp").text("Current Temperature is: " + weather.main.temp + " °F");
-        $("#cityHumidity").text("Current Humidity is: " + weather.main.humidity + "%");
-        $("#cityWind").text("Current Wind Speed is: " + weather.wind.speed + " MPH");  
+            // 1. Top Line Weather
+            // https://openweathermap.org/current - documentation for API call for Current Weather
+            // weather data query
+            $.ajax({
+                url: myQueryAPI,
+                method: "GET"
+            }).then(function(weather){  // pass in weather object from return of the myQueryAPI call
+                // using dayJS - show current time dayjs().format() 
+                // console.log("Today is: ", dayjs().format('dddd, MMMM D YYYY' +  ' h mm A'));
+                console.log("Today is: ", dayjs().format('dddd, MMMM D YYYY' +  ' h:mm A'));
+                var currentDateTime = dayjs().format('ddd, M/D/YYYY');
+                console.log(currentDateTime);
+                console.log("current query url is: ", myQueryAPI); // URL query request for weather data
+                console.log("weather data received from request url : ", weather); // object data received from request
+                // adding the City name to render in the html from the weather object - placement in id=#cityName
+                var showDate = $("<h4>"); // jQuery, go make me a h4 tag, & create variable to store element object to render in html markup
+                $("#cityName").empty(); // clear any preExisting data
+                $("#cityName").append(showDate.text(currentDateTime)); // jQuery, go find id and add date into its html tag the id lives
+                var showCity = $("<h4>").text(weather.name); // create variable to store object property weather.name to render in html markup
+                $("#cityName").prepend(showCity)// jQuery, go find id in html and now render in html markup in the beginning of selected tag
+                // adding the City weather icon to render in the html from the weather object - placement in id=#cityWeatherIcon
+                // https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon - FYI: how to overview to implement rendering the icon
+                var showIcon = $("<img>").attr("src","https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png");
+                $("#cityWeatherIcon").empty(); // clear any preExisting data
+                $("#cityWeatherIcon").append(showIcon); // render to html at id = #cityWeatherIcon
+                // adding in the temperature, humidity, windSpeed & UV index
+                $("#cityTemp").text("Current Temperature is: " + weather.main.temp + " °F");
+                $("#cityHumidity").text("Current Humidity is: " + weather.main.humidity + "%");
+                $("#cityWind").text("Current Wind Speed is: " + weather.wind.speed + " MPH");  
         
 
-        // 2. UV index:
-        // https://openweathermap.org/api/uvi  - documentation
-        // UV index is a different API query that requires the Longitude & Latitude coordinates:
-        // needs another Ajax call to get this data
-        longitude = weather.coord.lon; // set variable using coor.lon in the weather object from initial myQueryAPI call
-        latitude  = weather.coord.lat; // set variable using coor.lon in the weather object from initial myQueryAPI call
-        console.log("longitude & latitude data is: ", longitude, latitude);
-        // https://openweathermap.org/api/uvi  - documentation
-        // concatenate Longitude & Latitude to updated API call for UV data
-        var UVQueryAPI = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat="+latitude+"&lon="+longitude+"&appid="+myAPIkey;
-        console.log("UVQueryAPI is: ", UVQueryAPI);
-        // now make another ajax call:
-        $.ajax({
-            url: UVQueryAPI,  // silly typo - url cannont be URL - it throws aAccess to XMLHttpRequest from origin 'null' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, chrome-untrusted, https.
-            method: "GET"
-        }).then (function(uvIndex){  // UV Index data for our promise function to work on
-            console.log("UV data object is: ", uvIndex);
-            console.log("UVIndex array at sample time positions: 0,1,2,3,4,5,6,7: ", uvIndex[0].value +', '+
-                                                                                        uvIndex[1].value +', '+
-                                                                                        uvIndex[2].value +', '+
-                                                                                        uvIndex[3].value +', '+
-                                                                                        uvIndex[4].value +', '+
-                                                                                        uvIndex[5].value +', '+
-                                                                                        uvIndex[6].value );
-            $("#cityUV").text("Current UV index is: " + uvIndex[3].value);
-            // To do: 
-            // to add background color (using CSS) to text area, indicating favorable, moderate, or severe 
-            // build out conditional if statement that decides what is favorable, moderate, or severe - need to look up 
-            // favorable, moderate, or severe UV ranges.                                                                                              
-        });
-    
-        
-        // 5 Day Forcast:
+                // 2. UV index:
+                // https://openweathermap.org/api/uvi  - documentation for API call for UV Index
+                // UV index is a different API query that requires the Longitude & Latitude coordinates:
+                // needs another Ajax call to get this data
+                longitude = weather.coord.lon; // set variable using coor.lon in the weather object from initial myQueryAPI call
+                latitude  = weather.coord.lat; // set variable using coor.lon in the weather object from initial myQueryAPI call
+                console.log("longitude & latitude data is: ", longitude, latitude);
+                // https://openweathermap.org/api/uvi  - documentation
+                // concatenate Longitude & Latitude to updated API call for UV data
+                var UVQueryAPI = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat="+latitude+"&lon="+longitude+"&appid="+myAPIkey;
+                console.log("UVQueryAPI is: ", UVQueryAPI);
+                // now make another ajax call:
+                $.ajax({
+                    url: UVQueryAPI,  // silly typo - url cannont be URL - it throws aAccess to XMLHttpRequest from origin 'null' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, chrome-untrusted, https.
+                    method: "GET"
+                }).then (function(uvIndex){  // UV Index data for our promise function to work on
+                    console.log("UV data object is: ", uvIndex);
+                    console.log("UVIndex array at sample time positions: 0,1,2,3,4,5,6,7: ", uvIndex[0].value +', '+
+                                                                                                uvIndex[1].value +', '+
+                                                                                                uvIndex[2].value +', '+
+                                                                                                uvIndex[3].value +', '+
+                                                                                                uvIndex[4].value +', '+
+                                                                                                uvIndex[5].value +', '+
+                                                                                                uvIndex[6].value );
+                    $("#cityUV").text("Current UV index is: " + uvIndex[3].value);
+                    // To do: 
+                    // to add background color (using CSS) to text area, indicating favorable, moderate, or severe 
+                    // build out conditional if statement that decides what is favorable, moderate, or severe - need to look up 
+                    // favorable, moderate, or severe UV ranges. 
+
+               
+                        // 3. Five Day Forcast:
+                        // https://openweathermap.org/forecast5 - documentation for API call for 5-day forcast
+                        var fiveDayForecastAPI = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=e012fbc5ba9141b98a8748d19623cffb&q="+city;
+                                                    
+                        console.log("The 5 Day forcast API call is: ", fiveDayForecastAPI);
+                        // now make another ajax call:
+                        $.ajax({
+                            url: fiveDayForecastAPI,
+                            method: "GET"                    
+                        }).then (function(forecast){ // what is coming into this function when the promise resolves, store in fiveDayForcast object
+                            console.log("The 5 Day forcast object from API is: ", forecast);
+                            // 5 day forcast array represents weather data every 3hrs, I will target noon-time for each day of the 5 day forcast
+                            // The loop will match noon time for each day given that each day is divided by 3hr intervals as show in the 0-39 indexed forcast.list array
+                            for (var i=0; i < forecast.list.length; i+=8){
+                                 var day = $("<h6>"); // jQuery make tag, put in day
+                                 var num = (i + 8)/8; // fudge num value so that it counts from 1 to 5 to match its id attribute date1,2,3,4,5
+                                 var numForDate = (i+8)-3; // puts me in the middle of the day at 3:00pm
+                                //  if (numForDate = 40){numForDate=39}
+                                 console.log("check that our loop counts correctly - its day: ", "date" + num);
+                                 $("#date" + num).empty(); // 
+                                 //  $("#date" + num).append(day.text(currentDateTime.add(1,"days").format("m/d/yyyy")));
+                                //  $("#date" + num).append(day.text(currentDateTime));
+                                console.log("num is: ", num);
+                                console.log("numForDate is: ", numForDate);
+                                console.log("date is: ", forecast.list[numForDate].dt_txt);
+                                 $("#date" + num).append(forecast.list[numForDate].dt_txt);
+                            };
+                        })
+                
+
+                                                                                                                
+                });
 
 
 
 
-
-
-
-
-
-        
-
-
-
-
-
-
-    });
-
-  }
+            });
+}
   
 
 
